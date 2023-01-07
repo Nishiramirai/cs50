b759b172f56cc53267afb5f341d8666796020e67
Chezemy <Chezemy@gmail.com>
2023-01-07 16:03:29 +0300

    Initial commit

--- old/week1/hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    //Gets name from the user and welcoming him
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s\n", name);
-}
